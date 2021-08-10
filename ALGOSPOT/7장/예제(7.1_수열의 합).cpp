056d0e760cc566f7ea0afe11fa4a83b7a6e307bb
9keyyyy <9keyyyy@gmail.com>
2021-08-11 04:00:57 +0900

    add 7.1 ex

---
+++ ALGOSPOT/7장/예제(7.1_수열의 합).cpp
@@ -0,0 +1,26 @@
+﻿
+/*
+분할 정복 예제 - 수열의 합 구하기
+
+fastSum(n)
+
+= 1+2+3+...+n
+
+= (1+2+3+..+n/2) + ((n/2+1)+...+n)
+
+= (1+2+3+..+n/2) + (n/2+1) + (n/2+2) + ... + (n/2 + n/2)
+
+= (1+2+3+..+n/2) + n/2 * n/2 + (1+2+...+n/2)
+
+= fastSum(n/2) + n/2 * n/2 + fastSum(n/2)
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int fastSum(int n) {
+	if (n == 1) return 1;
+	if (n % 2 == 1) return fastSum(n - 1) + n;
+	return 2 * fastSum(n / 2) + n / 2 * n / 2;
+}
