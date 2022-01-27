5609772b4b99640e23ada53fbf888da53653f8f2
9keyyyy <9keyyyy@gmail.com>
2022-01-28 04:46:33 +0900

    add BOJ 11726

---
+++ BOJ/11726_2xn 타일링.cpp
@@ -0,0 +1,25 @@
+﻿// 22.01.28 BOJ 11726 2xn 타일링
+// 동적 계획법
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int cache[1001];
+int tiling(int n) {
+	// basecase
+	if (n <= 2) return n;
+
+	int& ret = cache[n];
+	if (ret != -1) return ret;
+
+	return ret = (tiling(n - 1) + tiling(n - 2)) % 10007;
+}
+int main() {
+	int n;
+
+	memset(cache, -1, sizeof(cache));
+	cin >> n;
+	cout << tiling(n) ;
+
+	return 0;
+}
