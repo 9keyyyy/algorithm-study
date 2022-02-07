e4ea2d0c1b40fc2772ccc8543cfb737426634042
9keyyyy <9keyyyy@gmail.com>
2022-02-07 21:54:43 +0900

    add BOJ 11047

---
+++ BOJ/11047_동전 0.cpp
@@ -0,0 +1,27 @@
+﻿// 2022-02-07 BOJ 11047 동전 0
+// 탐욕법
+#include <iostream>
+using namespace std;
+
+int N, K;
+int value[10];
+
+int calculateMin() {
+	int sum = 0;
+	for (int i = N - 1; i >= 0; i--) {
+		if (K < value[i]) continue;
+		sum += K / value[i];
+		K %= value[i];
+		if (K == 0) break;
+	}
+	return sum;
+}
+
+int main() {
+	cin >> N >> K;
+	
+	for (int i = 0; i < N; i++) cin >> value[i];
+	cout << calculateMin();
+
+	return 0;
+}
