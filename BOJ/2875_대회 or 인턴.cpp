cd7e6c1e4b7442783f0282bb0f26046d83eaf892
9keyyyy <9keyyyy@gmail.com>
2022-02-12 13:39:08 +0900

    add BOJ 2875

---
+++ BOJ/2875_대회 or 인턴.cpp
@@ -0,0 +1,19 @@
+﻿// 20220212 BOJ 2875 
+// 그리디
+#include <iostream>
+using namespace std;
+
+int main() {
+	int N, M, K, sum = 0;
+	cin >> N >> M >> K;
+
+	while (N >= 2 && M >= 1) {
+		N -= 2;
+		M -= 1;
+		if (N + M >= K) {
+			sum++;
+		}
+	}
+	cout << sum;
+	return 0;
+}
