c75ae6a33ba5ce0d4359ef31630891960cbd58de
9keyyyy <9keyyyy@gmail.com>
2022-03-31 14:25:59 +0900

    add BOJ 11651

---
+++ BOJ/11651_좌표 정렬하기 2.cpp
@@ -0,0 +1,27 @@
+﻿// 20220331 BOJ 좌표 정렬하기 2
+// 정렬
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+
+	int N, x, y;
+	vector<pair<int, int>> cor;
+
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> x >> y;
+		cor.push_back(make_pair(y, x));
+	}
+
+	sort(cor.begin(), cor.end());
+	
+	for (int i = 0; i < N; i++) {
+		cout << cor[i].second << " " << cor[i].first << "\n";
+	}
+	
+	return 0;
+}
