2cd1f76f058d895c2354b7131f300f76d277a2b2
9keyyyy <9keyyyy@gmail.com>
2022-04-20 00:19:09 +0900

    add BOJ 11053

---
+++ BOJ/11053_가장 긴 증가하는 부분 수열.cpp
@@ -0,0 +1,38 @@
+﻿//20220419
+//DP
+using namespace std;
+#include <iostream>
+#include <vector>
+
+int N, arr[1000], cache[1000] = { 0 };
+
+int returnSeq(int cur) {
+	if (cur == N - 1) return 0;
+	int& ret = cache[cur];
+	if (ret != 0) return ret;
+
+	for (int i = cur + 1; i < N; i++) {
+		if (arr[cur] < arr[i]) {
+			int res = 1 + returnSeq(i);
+			if (ret < res) ret = res;
+		}
+	}
+	return ret;
+
+}
+
+int main() {
+	
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> arr[i];
+	}
+	int max = 0;
+	for (int i = 0; i < N; i++) {
+		int res = returnSeq(i);
+		if (max < res) max = res;
+	}
+
+	cout << max + 1;
+	return 0;
+}
