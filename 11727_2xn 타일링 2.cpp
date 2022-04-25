40f605838c74abfc73563d4f4c18699fb46bd750
9keyyyy <9keyyyy@gmail.com>
2022-04-25 16:20:34 +0900

    add BOJ 11727

---
+++ 11727_2xn 타일링 2.cpp
@@ -0,0 +1,25 @@
+﻿//20220425
+//DP
+using namespace std;
+#include <iostream>
+
+int n;
+int cache[1001] = { 0 };
+
+int returnRec(int here) {
+	int& ret = cache[here];
+	
+	if (here <= 2 || ret != 0) return ret;
+	
+	ret = (returnRec(here - 1) + 2 * returnRec(here - 2)) % 10007;
+	return ret;
+}
+
+int main() {
+	cache[1] = 1;
+	cache[2] = 3;
+
+	cin >> n;
+	cout << returnRec(n);
+	return 0;
+}
