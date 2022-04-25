4b0bb81a347e82f3a5fdb4b1085c2b4e3d6cb708
9keyyyy <9keyyyy@gmail.com>
2022-04-25 16:20:21 +0900

    add BOJ 1463

---
+++ BOJ/1463_1로 만들기.cpp
@@ -0,0 +1,26 @@
+﻿//20220421
+//DP
+using namespace std;
+#include <iostream>
+#include <math.h>
+int N, cache[1000001] = { 0 };
+
+int findCnt(int num) {
+	if (num == 1) return 0;
+	
+	int& ret = cache[num];
+	if (ret != 0) return ret;
+
+	int res = 1 + findCnt(num - 1);
+	if (num % 3 == 0) res = min(res, 1 + findCnt(num / 3));
+	if (num % 2 == 0) res = min(res, 1 + findCnt(num / 2));
+
+	ret = res;
+	return ret;
+}
+int main() {
+	cin >> N;
+	
+	cout << findCnt(N);
+	return 0;
+}
