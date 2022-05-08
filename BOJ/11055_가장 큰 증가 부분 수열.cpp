e5388b85738c74dd3a4c5d638b22a0cc040b65dd
9keyyyy <9keyyyy@gmail.com>
2022-05-08 16:49:50 +0900

    add BOJ 11055

---
+++ BOJ/11055_가장 큰 증가 부분 수열.cpp
@@ -0,0 +1,38 @@
+﻿// 20220508
+// DP
+using namespace std;
+#include <iostream>
+
+int cache[1001];
+int arr[1001];
+int N;
+
+int returnMaxSeq(int idx) {
+	
+	if (idx == N - 1) return arr[idx];
+	int& ret = cache[idx];
+	if (ret != 0) return ret;
+	
+	int res, max = arr[idx];
+	for (int i = idx + 1; i < N; i++) {
+		if (arr[idx] < arr[i]) {
+			res = arr[idx] + returnMaxSeq(i);
+			if (res > max) max = res;
+		}
+	}
+	return ret = max;
+}
+
+int main() {
+	cin >> N;
+	
+	for (int i = 0; i < N; i++) cin >> arr[i];
+
+	int res,max = 0;
+	for (int i = 0; i < N; i++) {
+		res = returnMaxSeq(i);
+		if (res > max) max = res;
+	}
+	cout << max;
+	return 0;
+}
