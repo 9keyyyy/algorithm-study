463ec00b32744166ed049b9f43a903942e1600c8
9keyyyy <9keyyyy@gmail.com>
2022-05-08 17:20:03 +0900

    add BOJ 11722

---
+++ BOJ/11722_가장 긴 감소하는 부분 수열.cpp
@@ -0,0 +1,30 @@
+﻿// 20220508
+// DP
+using namespace std;
+#include <iostream>
+#include <math.h>
+int cache[1001];
+int arr[1001];
+int N;
+
+int sol() {
+	int m = 0;
+	for (int i = 0; i < N; i++) {
+		cache[i] = 1;
+		for (int j = 0; j < i; j++) {
+			if (arr[i] < arr[j]) {
+				cache[i] = max(cache[j] + 1, cache[i]);
+			}
+		}
+		m = max(m, cache[i]);
+	}
+	return m;
+}
+int main() {
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> arr[i];
+	}
+	cout << sol();
+	return 0;
+}
