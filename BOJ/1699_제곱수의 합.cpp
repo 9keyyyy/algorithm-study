08f13a4f03671d8f03541035ffaa4f128426b2c6
9keyyyy <9keyyyy@gmail.com>
2022-05-10 00:03:27 +0900

    add BOJ 1699

---
+++ BOJ/1699_제곱수의 합.cpp
@@ -0,0 +1,22 @@
+﻿// 20220509
+// DP
+using namespace std;
+#include <iostream>
+#include <math.h>
+int N;
+int cache[100001];
+
+int main() {
+	cin >> N;
+
+	for (int i = 1; i <= N; i++) {
+		cache[i] = i;
+		for (int j = 1; j * j <= i; j++) {
+			cache[i] = min(cache[i], cache[i - j * j] + 1);
+		}
+	}
+
+	cout << cache[N];
+
+	return 0;
+}
