9fb9127ae0d1d4fdf35f1d147b2810f5541bc0ed
9keyyyy <9keyyyy@gmail.com>
2022-05-09 23:23:54 +0900

    add BOJ 2579

---
+++ BOJ/2579_계단 오르기.cpp
@@ -0,0 +1,26 @@
+﻿// 20220509
+// DP
+using namespace std;
+#include <iostream>
+#include <math.h>
+int stair[301] = { 0 };
+int cache[301] = { 0 };
+int N;
+
+int sol() {
+	cache[0] = stair[0];
+	cache[1] = stair[0] + stair[1];
+	cache[2] = max(stair[2] + stair[0], stair[2] + stair[1]);
+	for (int i = 3; i < N; i++) {
+		cache[i] = max(cache[i - 2] + stair[i], cache[i - 3] + stair[i - 1] + stair[i]);
+	}
+	return cache[N - 1];
+}
+
+int main() {
+
+	cin >> N;
+	for (int i = 0; i < N; i++) cin >> stair[i];
+	cout << sol();
+	return 0;
+}
