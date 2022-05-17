fc953922fce17909deaff6a5d1cd19836e3bd3f6
9keyyyy <9keyyyy@gmail.com>
2022-05-17 19:27:21 +0900

    add BOJ 1991

---
+++ BOJ/1991_트리 순회.cpp
@@ -0,0 +1,45 @@
+﻿// 20220517
+// tree
+using namespace std;
+#include <iostream>
+
+int n;
+pair<int, int> edge[26];
+
+void preorder(char c) {
+	if (c == '.') return;
+	cout << c;
+	preorder(edge[c - 'A'].first);
+	preorder(edge[c - 'A'].second);
+}
+void inorder(char c) {
+	if (c == '.') return;
+	inorder(edge[c - 'A'].first);
+	cout << c;
+	inorder(edge[c - 'A'].second);
+}
+void postorder(char c) {
+	if (c == '.') return;
+	postorder(edge[c - 'A'].first);
+	postorder(edge[c - 'A'].second);
+	cout << c;
+}
+int main() {
+	char c, r, l;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> c >> l >> r;
+		edge[c - 'A'] = make_pair(l, r);
+	}
+
+	//for (int i = 0; i < n; i++) {
+	//	printf("%c %c %c\n", i + 'A', edge[i].first, edge[i].second);
+	//}
+
+	preorder('A');
+	cout << "\n";
+	inorder('A');
+	cout << "\n";
+	postorder('A');
+	cout << "\n";
+}
