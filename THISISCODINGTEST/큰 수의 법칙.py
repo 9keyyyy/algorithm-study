2ba6d0a66eb819b8812c8e0ac19520dc769a40f6
9keyyyy <9keyyyy@gmail.com>
2023-01-21 20:05:41 +0900

    add chap3 prob2 sol2

--- THISISCODINGTEST/큰 수의 법칙.py
+++ THISISCODINGTEST/큰 수의 법칙.py
@@ -1,5 +1,7 @@
 # greedy
 # point : 가장 큰 수, 두 번째로 큰 수 저장
+'''
+# sol 1
 
 n, m, k = map(int, input().split())
 data = list(map(int, input().split()))
@@ -18,5 +20,23 @@ while True:
     if m == 0: break
     result += second
     m -= 1
+
+print(result)
+'''
+
+# sol2
+n, m, k = map(int, input().split())
+data = list(map(int, input().split()))
+
+data.sort()
+first = data[n - 1]
+second = data[n - 2]
+
+count = int(m / (k + 1)) * k
+count += m % (k + 1)
+
+result = 0
+result += (count) * first
+result += (m - count) * second
 
 print(result)
