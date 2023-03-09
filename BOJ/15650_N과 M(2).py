02ca88aa4210470a14014af73efc4d2828b2ef02
9keyyyy <9keyyyy@gmail.com>
2023-03-09 22:22:56 +0900

    edit BOJ 15650

--- BOJ/15650_N과 M(2).py
+++ BOJ/15650_N과 M(2).py
@@ -3,7 +3,6 @@
 
 n, m = map(int, input().split())
 s = []
-visited = [False] * (n+1)
 
 def dfs(idx):
     if len(s) == m:
@@ -11,14 +10,9 @@ def dfs(idx):
         return
     
     for i in range(idx, n+1):
-        if visited[i]:
-            continue
-
         s.append(i)
-        visited[i] = True
         dfs(i + 1)
         s.pop()
-        visited[i] = False
 
 
 dfs(1)
