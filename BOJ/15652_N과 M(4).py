76f1624a8219aff7eaa9faace3d85c10465c168d
9keyyyy <9keyyyy@gmail.com>
2023-03-09 22:23:21 +0900

    add BOJ 15652

---
+++ BOJ/15652_N과 M(4).py
@@ -0,0 +1,17 @@
+# 230309
+# 백트래킹
+
+n, m = map(int, input().split())
+s = []
+
+def dfs(idx):
+    if len(s) == m:
+        print(' '.join(map(str, s)))
+        return
+    
+    for i in range(idx, n+1):
+        s.append(i)
+        dfs(i)
+        s.pop()
+
+dfs(1)
