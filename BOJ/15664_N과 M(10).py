5143b8687915327e34c111c9faee433455aa0a27
9keyyyy <9keyyyy@gmail.com>
2023-05-16 23:43:08 +0900

    add BOJ 15664

---
+++ BOJ/15664_N과 M(10).py
@@ -0,0 +1,27 @@
+# 230516
+# 백트래킹
+
+n, m = map(int, input().split())
+
+arr = list(map(int, input().split()))
+s = []
+visited = [False]*(n+1)
+
+arr.sort()
+
+def dfs(idx):
+    if len(s) == m:
+        print(' '.join(map(str, s)))
+        return
+    before = 0
+    for i in range(idx, len(arr)):
+        if visited[i] or before == arr[i]:
+            continue
+        visited[i] = True
+        before = arr[i]
+        s.append(arr[i])
+        dfs(i)
+        visited[i] = False
+        s.pop()
+
+dfs(0)
