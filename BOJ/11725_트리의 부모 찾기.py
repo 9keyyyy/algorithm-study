f1a79df2e86a3caf0def9435a6318489cb9e62de
9keyyyy <9keyyyy@gmail.com>
2023-05-29 23:24:27 +0900

    add BOJ 11725

---
+++ BOJ/11725_트리의 부모 찾기.py
@@ -0,0 +1,31 @@
+# 230529
+from collections import deque
+
+def bfs(idx):
+    q = deque()
+    q.append(idx)
+
+    while q:
+        cur = q.popleft()
+        for node in graph[cur]:
+            if parent[node] == 0:
+                parent[node] = cur
+                q.append(node)
+
+
+
+n = int(input())
+graph = [[] for _ in range(n+1)]
+parent = [0] * (n+1)
+
+# print(graph)
+for i in range(n-1):
+    s, e = map(int, input().split())
+    graph[s].append(e)
+    graph[e].append(s)
+
+bfs(1)
+
+for i in range(2, n+1):
+    print(parent[i])
+# print(graph)
