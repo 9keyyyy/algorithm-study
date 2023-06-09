4848790b40db0653a853117fa2148b86893285d7
9keyyyy <9keyyyy@gmail.com>
2023-06-10 02:54:48 +0900

    edit BOJ 2559

--- BOJ/2559_수열.py
+++ BOJ/2559_수열.py
@@ -1,2 +1,20 @@
 # 230609
 # 누적합
+
+n, k = map(int, input().split())
+arr = list(map(int, input().split()))
+
+total = 0
+for i in range(k):
+    total += arr[i]
+
+max_value = total
+
+for i in range(k, n):
+    total += arr[i]
+    total -= arr[i-k]
+
+    if total > max_value:
+        max_value = total
+
+print(max_value)
