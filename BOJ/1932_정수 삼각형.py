a0362d72a22c61c51a08d52547ecd0171b131255
9keyyyy <9keyyyy@gmail.com>
2023-06-24 02:31:32 +0900

    add BOJ 1932

---
+++ BOJ/1932_정수 삼각형.py
@@ -0,0 +1,18 @@
+# 230624
+# DP
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+triangle = [list(map(int, input().split())) for _ in range(n)]
+dp = [[0]*(n) for _ in range(n)]
+dp[0][0] = triangle[0][0]
+
+for i in range(n - 1):
+    for j in range(len(triangle[i])):
+        dp[i+1][j] = max(dp[i+1][j], dp[i][j] + triangle[i+1][j])
+        dp[i+1][j+1] = max(dp[i+1][j+1], dp[i][j] + triangle[i+1][j+1])
+
+for i in range(n):
+    print(dp[i])
+print(max(dp[n-1]))
