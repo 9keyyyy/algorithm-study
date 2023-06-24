72d2b5af961117de948aec3825653f2687112946
9keyyyy <9keyyyy@gmail.com>
2023-06-24 12:49:53 +0900

    edit BOJ 1932

--- BOJ/1932_정수 삼각형.py
+++ BOJ/1932_정수 삼각형.py
@@ -13,6 +13,5 @@ for i in range(n - 1):
         dp[i+1][j] = max(dp[i+1][j], dp[i][j] + triangle[i+1][j])
         dp[i+1][j+1] = max(dp[i+1][j+1], dp[i][j] + triangle[i+1][j+1])
 
-for i in range(n):
-    print(dp[i])
+
 print(max(dp[n-1]))
