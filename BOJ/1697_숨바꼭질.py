74d3de8c9be82b9fc5b93ec12e9c6ca605d47788
9keyyyy <9keyyyy@gmail.com>
2023-06-27 00:13:40 +0900

    edit BOJ 1697

--- BOJ/1697_숨바꼭질.py
+++ BOJ/1697_숨바꼭질.py
@@ -7,31 +7,26 @@ input = sys.stdin.readline
 from collections import deque
 
 n, k = map(int, input().split())
-arr = [-1]*(k+2)
-arr[n] = 0
+arr = [0]*(100001)
 
 q = deque()
 q.append(n)
 
-
 while q:
     idx = q.popleft()
-    # print("idx" , idx)
-    # print(arr)
+
     if idx == k:
         print(arr[idx])
         break
 
-    if idx - 1 >= 0 and arr[idx - 1] == -1:
+    if 0 <= idx - 1 <= 100000 and arr[idx - 1] == 0:
         arr[idx - 1] = arr[idx] + 1
         q.append(idx - 1)
     
-    if idx + 1 <= k + 1 and arr[idx + 1] == -1:
+    if 0 <= idx + 1 <= 100000 and arr[idx + 1] == 0:
         arr[idx + 1] = arr[idx] + 1
         q.append(idx + 1)
         
-    if idx * 2 <= k + 1 and arr[idx * 2] == -1:
+    if 0 <= idx * 2 <= 100000 and arr[idx * 2] == 0:
         arr[idx * 2] = arr[idx] + 1
         q.append(idx * 2)
-    
-# print(arr)
