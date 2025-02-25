d1b4bd2b26e98a2cb15b126d8cdcd6543e403d1a
9keyyyy <62213813+9keyyyy@users.noreply.github.com>
2025-02-25 18:44:24 +0900

    add programmers 최고의 집합.py

---
+++ PROGRAMMERS/최고의 집합.py
@@ -0,0 +1,18 @@
+# 20250225
+# 구현
+
+def solution(n, s):
+    answer = []
+    
+    if (n > s):
+        answer = [-1]
+    else:
+        for _ in range(n):
+            answer.append(s // n)
+
+        for i in range(s % n):
+            answer[i] += 1
+            
+        answer.sort()
+        
+    return answer
